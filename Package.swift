fee71bfac7ecf7e585c9399450c007968cabc545
John Ellie Go <ellie.go12@gmail.com>
2022-11-23 17:38:46 +0800

    Added package metadata

--- Package.swift
+++ Package.swift
@@ -11,18 +11,10 @@ let package = Package(
             name: "GoogleCast",
             targets: ["GoogleCast"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "GoogleCast",
-            dependencies: []),
-        .testTarget(
-            name: "GoogleCastTests",
-            dependencies: ["GoogleCast"]),
+            path: "Frameworks/GoogleCast.xcframework"
+        )
     ]
 )
